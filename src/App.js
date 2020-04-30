23df1281a73abf8aa1db535e316f0572899ecc23
Klara Eserstam <klara.eserstam@gmail.com>
2020-04-30 09:14:21 +0200

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import asta from './asta.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={asta} className="App-logo" alt="logo" />
         <p>
-          Hej hej hej
+          Vår fina app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
