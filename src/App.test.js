c51576be6623674e37b2ef59b8ee195cd0d3cd76
Klara Eserstam <klara.eserstam@gmail.com>
2020-04-30 10:36:54 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/fina app/i);
+  const linkElement = getByText(/hej/i);
   expect(linkElement).toBeInTheDocument();
 });
